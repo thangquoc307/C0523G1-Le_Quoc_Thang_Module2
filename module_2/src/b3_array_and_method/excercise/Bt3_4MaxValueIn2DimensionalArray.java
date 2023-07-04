42afb9a52b06f31c870741da26733c3a6f4d8526
Thắng Lê <lequocthang307@gmail.com>
2023-07-04 15:31:24 +0700

    adgb

--- module_2/src/b3_array_and_method/excercise/Bt3_4MaxValueIn2DimensionalArray.java
+++ module_2/src/b3_array_and_method/excercise/Bt3_4MaxValueIn2DimensionalArray.java
@@ -1,10 +1,10 @@
 package b3_array_and_method.excercise;
 
-public class Bt3_4MinValueIn2DimensionalArray {
+public class Bt3_4MaxValueIn2DimensionalArray {
     public static void main(String[] args) {
         int[][] array = {{8,2,5,7}, {6,7,8,9,6,4}, {2,4,5,7,7,9,9,6,4,3}};
         print(array);
-        System.out.println("Min in Array is " + min(array));
+        System.out.println("Min in Array is " + max(array));
     }
     public static void print(int[][]arr){
         for (int i = 0; i < arr.length; i++){
@@ -17,15 +17,15 @@ public class Bt3_4MinValueIn2DimensionalArray {
             }
         }
     }
-    public static int min(int[][]arr){
-        int min = arr[0][0];
+    public static int max(int[][]arr){
+        int max = arr[0][0];
         for (int i = 0; i < arr.length; i++){
             for (int j = 0; j < arr[i].length; j++){
-                if (min > arr[i][j]){
-                    min = arr[i][j];
+                if (max < arr[i][j]){
+                    max = arr[i][j];
                 }
             }
         }
-        return min;
+        return max;
     }
 }
