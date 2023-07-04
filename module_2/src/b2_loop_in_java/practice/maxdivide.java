6a2ee0a977cb91f059965376b48b09aeb60b2516
Thắng Lê <lequocthang307@gmail.com>
2023-07-04 11:51:45 +0700

    adfb

---
+++ module_2/src/b2_loop_in_java/practice/maxdivide.java
@@ -0,0 +1,28 @@
+package b2_loop_in_java.practice;
+
+import java.util.Scanner;
+
+public class maxdivide {
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a: ");
+        a = input.nextInt();
+        System.out.println("Enter b: ");
+        b = input.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        while (a != b) {
+            if (a > b)
+                a = a - b;
+            else
+                b = b - a;
+        }
+
+        System.out.println("Greatest common factor: " + a);
+    }
+}
