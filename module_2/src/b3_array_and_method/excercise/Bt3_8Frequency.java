6fe2056e7620d5b9be892c65e7a1b8acf6306f70
Thắng Lê <lequocthang307@gmail.com>
2023-07-04 16:39:00 +0700

    dfb

---
+++ module_2/src/b3_array_and_method/excercise/Bt3_8Frequency.java
@@ -0,0 +1,21 @@
+package b3_array_and_method.excercise;
+
+import java.util.Scanner;
+
+public class Bt3_8Frequency {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mời nhập vào chuỗi ký tự : ");
+        String str = scanner.nextLine();
+        System.out.println("Bạn muốn kiếm ký tự nào : ");
+        char key = scanner.next().charAt(0);
+        int count = 0;
+
+        for (int i = 0; i < str.length(); i++){
+            if(key == str.charAt(i)){
+                count++;
+            }
+        }
+        System.out.println("Ký tự : " + key + " xuất hiện " + count + " lần");
+    }
+}
