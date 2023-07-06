181719aa94aafb875460570c33d075ac0cd58021
Thắng Lê <lequocthang307@gmail.com>
2023-07-06 10:29:17 +0700

    ghjk

---
+++ module_2/src/b6_inheritance/excercise/Bt6_1CircleAndCylinder.java
@@ -0,0 +1,77 @@
+package b6_inheritance.excercise;
+
+public class Bt6_1CircleAndCylinder {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle(10, "White");
+
+        System.out.println(circle1.getStatus());
+        System.out.println(circle2.getStatus());
+
+        Cylinder cylinder1 = new Cylinder();
+        Cylinder cylinder2 = new Cylinder(18, 15, "Brown");
+
+        System.out.println(cylinder1.getStatus());
+        System.out.println(cylinder2.getStatus());
+
+
+    }
+    public static class Circle{
+        int radius = 5;
+        String colour = "Green";
+        public Circle(){}
+        public Circle(int radius, String colour){
+            this.colour = colour;
+            this.radius = radius;
+        }
+        public int getRadius() {
+            return this.radius;
+        }
+        public String getColour() {
+            return this.colour;
+        }
+        public void setRadius(int radius) {
+            this.radius = radius;
+        }
+        public void setColour(String colour) {
+            this.colour = colour;
+        }
+        public double getArea(){
+            return Math.PI * Math.pow(this.radius, 2);
+        }
+        public String getStatus(){
+            return "A circle has "
+                    + "\n  + radius : " + this.getRadius()
+                    + "\n  + colour : " + this.getColour()
+                    + "\n  + area : " + this.getArea();
+        }
+    }
+    public static class Cylinder extends Circle {
+        int height = 6;
+        public Cylinder(){}
+        public Cylinder(int radius, int height, String colour){
+            this.radius = radius;
+            this.height = height;
+            this.colour = colour;
+        }
+        public int getHeight() {
+            return this.height;
+        }
+        public void setHeight(int height) {
+            this.height = height;
+        }
+
+        public double getVolume() {
+            return this.getArea() * this.getHeight();
+        }
+
+        @Override
+        public String getStatus() {
+            return "A cylinder has "
+                    + "\n  + radius : " + this.getRadius()
+                    + "\n  + height : " + this.getHeight()
+                    + "\n  + colour : " + this.getColour()
+                    + "\n  + volume : " + this.getVolume();
+        }
+    }
+}
