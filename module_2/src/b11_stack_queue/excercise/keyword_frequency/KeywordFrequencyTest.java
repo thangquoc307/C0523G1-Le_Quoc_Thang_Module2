6d1be5252c5b6ee636f1042acfa9357c1dcad8f6
Thắng Lê <lequocthang307@gmail.com>
2023-07-12 14:48:51 +0700

    fb

---
+++ module_2/src/b11_stack_queue/excercise/keyword_frequency/KeywordFrequencyTest.java
@@ -0,0 +1,9 @@
+package b11_stack_queue.excercise.keyword_frequency;
+
+public class KeywordFrequencyTest {
+    public static void main(String[] args) {
+        Keyword string = new Keyword("sdfghjklkjhwrhiubhwnt;bj h0ywthi;obnhso;bnsryitb/j ưy9pmotagfds");
+        System.out.println(string.getStr());
+        string.countWord();
+    }
+}
