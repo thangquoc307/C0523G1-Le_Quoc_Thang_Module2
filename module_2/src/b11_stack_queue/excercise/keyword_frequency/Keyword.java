98e1e59b2afdb7bd981eae4721b82271c9972316
Thắng Lê <lequocthang307@gmail.com>
2023-07-12 15:01:42 +0700

    sfgb

--- module_2/src/b11_stack_queue/excercise/keyword_frequency/Keyword.java
+++ module_2/src/b11_stack_queue/excercise/keyword_frequency/Keyword.java
@@ -4,17 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 public class Keyword {
     private String str;
-    private Map<Character, Integer> listKey = new HashMap<>();
-
     public Keyword(String str) {
         this.str = str;
-        for (int i = 0; i < str.length(); i++){
-            if (!listKey.containsKey(str.charAt(i))){
-                listKey.put(str.charAt(i), 1);
-            }else {
-                listKey.put(str.charAt(i), listKey.get(str.charAt(i)) + 1);
-            }
-        }
     }
     public String getStr() {
         return str;
@@ -23,7 +14,14 @@ public class Keyword {
         this.str = str;
     }
     public void countWord(){
-
+        Map<Character, Integer> listKey = new HashMap<>();
+        for (int i = 0; i < str.length(); i++){
+            if (!listKey.containsKey(str.charAt(i))){
+                listKey.put(str.charAt(i), 1);
+            }else {
+                listKey.put(str.charAt(i), listKey.get(str.charAt(i)) + 1);
+            }
+        }
         for (Character key : listKey.keySet()){
             int value = listKey.get(key);
             System.out.println("Ký tự : " + key + " Xuất hiện " + value + " lần");
--- module_2/src/b11_stack_queue/excercise/keyword_frequency/KeywordFrequencyTest.java
+++ module_2/src/b11_stack_queue/excercise/keyword_frequency/KeywordFrequencyTest.java
@@ -2,8 +2,12 @@ package b11_stack_queue.excercise.keyword_frequency;
 
 public class KeywordFrequencyTest {
     public static void main(String[] args) {
-        Keyword string = new Keyword("sdfghjklkjhwrhiubhwnt;bj h0ywthi;obnhso;bnsryitb/j ưy9pmotagfds");
+        Keyword string = new Keyword("sdqqqqitb/j ưy9pmotagfds");
         System.out.println(string.getStr());
         string.countWord();
+        System.out.println("");
+        string.setStr("sgbgggggggg");
+        string.countWord();
+
     }
 }
