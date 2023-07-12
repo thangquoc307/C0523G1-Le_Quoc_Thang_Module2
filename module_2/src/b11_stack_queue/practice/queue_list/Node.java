4ef5eec81d35f9c40aa0decededda271ea848f47
Thắng Lê <lequocthang307@gmail.com>
2023-07-12 10:30:09 +0700

    jk

---
+++ module_2/src/b11_stack_queue/practice/queue_list/Node.java
@@ -0,0 +1,11 @@
+package b11_stack_queue.practice.queue_list;
+
+class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
