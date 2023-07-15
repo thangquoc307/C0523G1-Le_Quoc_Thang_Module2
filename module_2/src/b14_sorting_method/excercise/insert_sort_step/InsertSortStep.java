d0715548db44fab706219a6611334de0a58c7cfb
Thắng Lê <lequocthang307@gmail.com>
2023-07-15 10:48:37 +0700

    bg

---
+++ module_2/src/b14_sorting_method/excercise/insert_sort_step/InsertSortStep.java
@@ -0,0 +1,41 @@
+package b14_sorting_method.excercise.insert_sort_step;
+
+import sun.management.snmp.jvmmib.EnumJvmThreadContentionMonitoring;
+
+public class InsertSortStep {
+    public static void main(String[] args) {
+
+        int[] list = {66, 34, 26, 98, 10, 64, 38, 96, 35};
+        list = InsertSort(list);
+        System.out.println("");
+        System.out.println("Kết quả sau khi Sort");
+        print(list);
+    }
+    public static int[] InsertSort(int[] sortList) {
+        for (int i = 1; i < sortList.length; i++){
+            int insertItem = sortList[i];
+            for (int j = i - 1; j >= 0; j--){
+                if (insertItem >= sortList[j]){
+                    sortList[j + 1] = insertItem;
+                    break;
+                }else {
+                    sortList[j + 1] = sortList[j];
+                    sortList[j] = insertItem;
+                }
+            }
+
+            System.out.println("Lần chạy cho list index : " + i + " cho số " + insertItem);
+            print(sortList);
+            System.out.println("");
+            System.out.println("");
+
+        }
+        return sortList;
+    }
+    public static void print(int[] printList){
+        for(int number : printList){
+            System.out.print(number + " ");
+        }
+    }
+}
+
